4313bc37ea044f886f754b09efdfe4647a401d8d
Mostafa Magdy <mostafa.magdy621@gmail.com>
2025-03-05 14:31:04 +0200

    feat: authentication added to handle login and register new users

---
+++ src/routes/auth.router.ts
@@ -0,0 +1,43 @@
+import express from "express"
+import UserModel from "../models/User.model"
+import { logger } from "../config/Logger"
+import jwt from "jsonwebtoken"
+
+const router = express.Router();
+
+
+router.post("/register", (req, res) => {
+    logger.info("...register router...");
+    try {
+        const { userName, password } = req.body
+        const user = UserModel.create({
+            userName,
+            password
+        })
+        res.status(201).json({ message: 'User registered successfully' })
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Registration failed' });
+    }
+})
+
+router.post("/login", async (req, res) => {
+    logger.info("... login router ...");
+    try {
+        const { userName, password } = req.body
+        const user = await UserModel.findOne({ userName });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ message: 'userName or password not correct' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET ?? "", {
+            expiresIn: '1h',
+        });
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ message: "login failed" });
+    }
+})
+
+
+export default router;
